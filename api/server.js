6eeb8bf0b3510c12acb65b014da9f4df910b47df
Alexander Sierra <alexandercsierra@gmail.com>
2020-05-01 21:35:46 -0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
-const apiRouter = require('./apiRouter')
+const cors = require('cors');
+const apiRouter = require('./apiRouter');
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 server.use('/api', apiRouter);
 
